f01508acff09c0b72a52a45602f95f568ace9e77
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-06-25 23:40:24 +0600

    add: basic ts types added in practice file

---
+++ practice.ts
@@ -0,0 +1,12 @@
+let message: string = 'Hello world!';
+let age: number = 24;
+let job: boolean = false;
+let data: any = 'could be anything';
+let skills: string[] = ['html', 'css', 'js'];
+let numbers: number[] = [1, 2, 3, 4, 5];
+let user: [string, number] = ['sanny', 24];
+function logMessage(message: string): void {
+  console.log(message);
+}
+let emptyValue: null = null;
+let notAssigned: undefined = undefined;
