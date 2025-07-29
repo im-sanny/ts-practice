fdeea74b1057341544676401b39749f7db2c361f
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-07-29 23:47:22 +0600

    add: created practice file and added some ts types

---
+++ practice.ts
@@ -0,0 +1,7 @@
+let text: string = 'hello world!';
+let age1: number = 20;
+let adult: boolean = true;
+let dataType: any = 'write anything here!';
+let names: string[] = ['tom', 'jerry', 'butch', 'bulldog'];
+let nums: number[] = [1, 2, 3, 4];
+let person: [string, number] = ['Sanny', 69];
