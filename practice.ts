dddc874c64c70ae57f079b3bcb905bcae679f178
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-08-01 22:05:54 +0600

    add: added new type in practice file

--- practice.ts
+++ practice.ts
@@ -5,3 +5,10 @@ let dataType: any = 'write anything here!';
 let names: string[] = ['tom', 'jerry', 'butch', 'bulldog'];
 let nums: number[] = [1, 2, 3, 4];
 let person: [string, number] = ['Sanny', 69];
+
+function hello(message: string): void {
+  console.log(message);
+}
+
+let empty: null = null;
+let und: undefined = undefined;
